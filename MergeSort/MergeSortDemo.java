249d16c52f09f741bd6a0ea2c055da4da7d47802
jldeahr <jldeahr@NN123-HA-12.HS.sd203.org>
2015-04-07 12:20:06 -0500

    Broke a friendship today by accident lol

--- MergeSort/MergeSortDemo.java
+++ MergeSort/MergeSortDemo.java
@@ -8,7 +8,14 @@ public class MergeSortDemo
 {  
    public static void main(String[] args)
    {  
-      int[] a = ArrayUtil.randomIntArray(20, 100);
+      String[] a = new String[8];
+      a[0] = "these";
+      a[1] = "two";
+      a[2] = "next";
+      a[3] = "to";
+      a[4] = "me";
+      a[5] = "are";
+      a[6] = "useless";
       System.out.println(Arrays.toString(a));
 
       MergeSorter.sort(a);
